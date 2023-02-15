a959dcf4784ee0d0a416d16a08f0fd85d82039b8
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-02-15 15:34:40 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -49,8 +49,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", https://t.me/the_update_silencer)
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", https://t.me/x_team_immortals_x)
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", he_update_silencer)
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", x_team_immortals_x)
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
 
